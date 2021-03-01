01bbd2508277a930d54bb7e725923f0ac5095b91
Greg Price <greg@madebyhive.com>
2021-03-01 15:09:55 +0000

    Remove deletes post stash

--- RxRover/ViewController.swift
+++ RxRover/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // https://api.nasa.gov
+    
+    // 2 tabs = photos / favs
+    
+    // photos = feed of images from the api
+    // with a set of filters (for each rover, then for each camera?)
+    // use the cook grid built for alpacr?
+    // or maybe diffable data source? with cool layout?
+    
+    // tap onto photo goes to a detail screen
+    // add this to favs
+    
+    // favs = list of favs
+    // can remove from favs
+    // can view the detail (maybe detail has state add vs rmeove)
+    
+    // plus a page to show details for the rover (maybe a custom animtion for this? popover or something?)
+    
+    // do something cool with the color apis? (labels over photos etc)
+    // do some sort of transition from photo to detail? something cool instead of just push??
 }
 
